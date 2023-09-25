a4b57095775bca0a5a3f5eb6ff49e5dd289f6a64
boel21 <joelvergara21@gmail.com>
2023-09-24 20:57:56 -0500

    Complete frontend dashboard

---
+++ README.md
@@ -0,0 +1,25 @@
+# Sales Dashboard
+
+Sales dashboard made with vanilla JS, HTML5, CSS
+
+## Installation
+
+No packages required
+
+## Usage
+
+```javascript
+Just download and use any web server
+
+```
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first
+to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+  {
+    productName: "Foldable Drone",
+    productNumber: "85631",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Foldable Drone",
+    productNumber: "85631",
+    paymentStatus: "Refunded",
+    shipping: "Declined",
+  },
+  {
+    productName: "Foldable Drone",
+    productNumber: "56941",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Foldable Drone",
+    productNumber: "85631",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "GPS 4K Drone",
+    productNumber: "85631",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "Gaming Laptop",
+    productNumber: "69831",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Foldable Drone",
+    productNumber: "85631",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+];
